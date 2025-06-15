a192d61bf3aedff36b47cc73835675010af2047e
iwamatsu0430 <iwamatsu0430@gmail.com>
2025-06-15 23:05:48 +0900

    fix example

--- example/src/query/user.sql
+++ example/src/query/user.sql
@@ -4,8 +4,8 @@ SELECT name FROM user WHERE id = $1
 
 -- 5eed765f7bd9b64c48e08823ad321f38
 -- ユーザーを全件取得する
-SELECT id, name, age FROM user
+SELECT "id", "name", "age" FROM "user"
 
--- 1cc99f0e4ef8e0aa88d7ed06d7f43bd1
+-- 55c42cc0da4e4eb71fd33d70e6896277
 -- ユーザーの平均年齢を取得する
 SELECT AVG(age) FROM user
